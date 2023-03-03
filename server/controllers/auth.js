25f4496c3755b40164a620e0270e50afbc8e3b08
HopefulHeart2020 <chibayakuya@gmail.com>
2023-03-03 13:06:23 +0200

    login controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -36,3 +36,27 @@ export const register = async (req, res) => {
         res.status(500).json({error: err.message});
     }
 }
+
+// login user
+export const login = async (req, res) => {
+    try{
+        const {email, password} = req.body;
+        const user = await User.findOne({email:email});
+        if(!user) return res.status(400).json({msg: 'User does not exist'});
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({msg: 'Invalid credentials'});
+        }
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({
+            token,
+            user
+        });
+
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+}
