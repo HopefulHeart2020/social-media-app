e953d3b95c43ae856a496f6659ce6d4f294f6007
HopefulHeart2020 <chibayakuya@gmail.com>
2023-03-05 21:42:44 +0200

    material ui theme settings

--- client/src/theme.js
+++ client/src/theme.js
@@ -103,3 +103,78 @@ export const themeSettings = (mode) => {
         },
     };
 };
+
+//mui theme setting 
+
+export const themeSetting = (mode) => {
+    return {
+        pallete: {
+            mode: mode,
+            ...(mode === "dark"
+                ? {
+                    primary: {
+                        dark: colorTokens.primary[200],
+                        main: colorTokens.primary[500],
+                        light: colorTokens.primary[800],
+                    },
+                    neutral: {
+                        dark: colorTokens.grey[100],
+                        main: colorTokens.grey[200],
+                        mediumMain: colorTokens.grey[300],
+                        medium: colorTokens.grey[400],
+                        light: colorTokens.grey[700],
+                    },
+                    background: {
+                        default: colorTokens.grey[900],
+                        alt: colorTokens.grey[800],
+                    },
+                }
+                : {
+                    primary: {
+                        dark: colorTokens.primary[700],
+                        main: colorTokens.primary[500],
+                        light: colorTokens.primary[50],
+                    },
+                    neutral: {
+                        dark: colorTokens.grey[700],
+                        main: colorTokens.grey[500],
+                        mediumMain: colorTokens.grey[400],
+                        medium: colorTokens.grey[300],
+                        light: colorTokens.grey[50],
+                    },
+                    background: {
+                        default: colorTokens.grey[10],
+                        alt: colorTokens.grey[0],
+                    },
+                }),
+        },
+        typography: {
+            fontFamily: ["Rubik", "sans-serif"].join(","),
+            fontSize: 12,
+            h1: {
+                fontFamily: ["Rubik", "sans-serif"].join(","),
+                fontSize: 40,
+            },
+            h2: {
+                fontFamily: ["Rubik", "sans-serif"].join(","),
+                fontSize: 32,
+            },
+            h3: {
+                fontFamily: ["Rubik", "sans-serif"].join(","),
+                fontSize: 24,
+            },
+            h4: {
+                fontFamily: ["Rubik", "sans-serif"].join(","),
+                fontSize: 20,
+            },
+            h5: {
+                fontFamily: ["Rubik", "sans-serif"].join(","),
+                fontSize: 16,
+            },
+            h6: {
+                fontFamily: ["Rubik", "sans-serif"].join(","),
+                fontSize: 14,
+            },
+        },
+    }
+};
