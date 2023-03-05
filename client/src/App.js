3cb645ad0a0383d4b115d87e15af3c3f01d78353
HopefulHeart2020 <chibayakuya@gmail.com>
2023-03-05 21:00:45 +0200

    client setup & redux setup

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter ,Navigate , Routes, Route } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+
+
+function App() {
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userid" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
