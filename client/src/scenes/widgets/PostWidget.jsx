bec9caec391291a4117fb23a2b30d97ea2d95881
HopefulHeart2020 <chibayakuya@gmail.com>
2023-03-07 18:16:59 +0200

    finished working on friend preview

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,9 @@
+import {
+    ChatBubbleOutlineOutlined,
+    FavoriteBorderOutlined,
+    FavoriteOutlined,
+    ShareOutlined
+} from "@mui/icons-material"
+import { Box, Divider , IconButton,useTheme } from "@mui/material"
+import FlexBetween from "../../components/FlexBetween"
+import Friend from "../../components/Friend"
